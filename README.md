d62cf2db123413ecbdbafa8373b57dc538998847
kherin bundhoo <kherinbundhoo@kherins-MacBook-Pro.local>
2020-07-12 22:13:26 +0400

    Added collision mechanism

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-# Using JavaScript to create a basic and experimental treasure hunt game
+# Using JavaScript to create a basic and experimental treasure hunt game (Building each part under ~30 minutes)
 
 ## Part 1
+
 ### Basic movements and counting number of treasures found
+
 ![](part_1.gif)
+
+## Part 2
+
+### Added obstacles and a collision mechanism
+
+![](part_2.gif)
--- sketch.js
+++ sketch.js
@@ -11,13 +11,36 @@ let userPositionY = 0;
 const numTreasureChests = 10;
 const treasures = [];
 
+// obstacles
+const obstacles = [];
+
+let showTreasureAlert = true;
 
 // number of treasure chests found
 const treasureCounter = 0;
+const positionsTaken = [];
 
 function getRandomPosition() {
-    let randomX = Math.floor(Math.random() * 10) * 30;
-    let randomY = Math.floor(Math.random() * 10) * 30;
+
+    let randomX = 0;
+    let randomY = 0;
+
+    while (true) {
+        randomX = Math.floor(Math.random() * 10) * 30;
+        randomY = Math.floor(Math.random() * 10) * 30;
+
+        if (positionsTaken.length) {
+            let positionTakenIndex = positionsTaken.findIndex((position) => position[0] == randomX && position[1] == randomY);
+            if (positionTakenIndex == -1) {
+                positionsTaken.push([randomX, randomY])
+                break;
+            }
+        } else {
+            positionsTaken.push([randomX, randomY])
+            break;
+        }
+
+    }
 
     return [randomX, randomY];
 }
@@ -29,6 +52,13 @@ function createTreasure() {
     }
 }
 
+function createObstacles() {
+    let numObstacles = Math.floor(Math.random() * 50);
+    for (let index = 0; index < numObstacles; index++) {
+        obstacles.push(getRandomPosition());
+    }
+}
+
 function setup() {
     createCanvas(300, 300);
     let [randomX, randomY] = getRandomPosition();
@@ -37,6 +67,12 @@ function setup() {
 
     //treasures
     createTreasure();
+
+    //create obstacles
+    createObstacles();
+
+    console.log('treasures: ', treasures);
+    console.log('obstacles: ', obstacles);
 }
 
 function draw() {
@@ -58,6 +94,9 @@ function draw() {
             } else if (isTreasure(width, height)) {
                 stroke(0);
                 fill(243, 148, 40);
+            } else if (isObstacle(width, height)) {
+                stroke(0);
+                fill(60, 60, 60);
             }
             rect(height, width, 30, 30);
         }
@@ -66,11 +105,23 @@ function draw() {
 
 function updateTreasureCounter() {
     document.querySelector('#treasureCounter').innerHTML = `Treasure Counter: ${numTreasureChests - treasures.length}`;
+    if (treasures.length == 0 && showTreasureAlert) {
+        showTreasureAlert = false;
+        alert("You've collected all the treasures!");
+    }
+}
+
+function isRelated(x, y, objects) {
+    const foundIndex = objects.findIndex(coordinates => coordinates[0] == x && coordinates[1] == y);
+    return foundIndex > -1;
 }
 
 function isTreasure(x, y) {
-    const foundIndex = treasures.findIndex(coordinates => coordinates[0] == x && coordinates[1] == y);
-    return foundIndex > -1;
+    return isRelated(x, y, treasures);
+}
+
+function isObstacle(x, y) {
+    return isRelated(x, y, obstacles);
 }
 
 function removeTreasure(x, y) {
@@ -82,23 +133,30 @@ function isWithinBoundary(value) {
     return value >= 0 && value <= 270;
 }
 
+function hitObstacle(x, y) {
+    return isRelated(x, y, obstacles);
+}
+
 function keyPressed() {
     if (keyCode == UP_KEY) {
-        if (isWithinBoundary(userPositionX - 30)) {
-            userPositionX = userPositionX - 30;
+        let nextMove = userPositionX - 30;
+        if (isWithinBoundary(nextMove) && !hitObstacle(nextMove, userPositionY)) {
+            userPositionX = nextMove;
         }
     } else if (keyCode == RIGHT_KEY) {
-        if (isWithinBoundary(userPositionY + 30)) {
-            userPositionY = userPositionY + 30;
+        let nextMove = userPositionY + 30;
+        if (isWithinBoundary(nextMove) && !hitObstacle(userPositionX, nextMove)) {
+            userPositionY = nextMove;
         }
     } else if (keyCode == DOWN_KEY) {
-        if (isWithinBoundary(userPositionX + 30)) {
-            userPositionX = userPositionX + 30;
+        let nextMove = userPositionX + 30;
+        if (isWithinBoundary(nextMove) && !hitObstacle(nextMove, userPositionY)) {
+            userPositionX = nextMove;
         }
     } else if (keyCode == LEFT_KEY) {
-        if (isWithinBoundary(userPositionY - 30)) {
-            userPositionY = userPositionY - 30;
+        let nextMove = userPositionY - 30;
+        if (isWithinBoundary(nextMove) && !hitObstacle(userPositionX, nextMove)) {
+            userPositionY = nextMove;
         }
     }
-
 }
